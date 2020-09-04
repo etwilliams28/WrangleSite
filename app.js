e46eb10d23e6f94c7a016839067befb701d41ade
Ethan_Williams <etwilliams28@gmail.com>
2020-09-04 13:58:33 -0700

    Update: Update get request with function to check if object is in db and to render object to showcase page.
    Add: Add database and schema and test object to db.
    
    Update: Update post request to pull inputs from console page.

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 // connecting to database
 mongoose.connect("mongodb://localhost:27017/wrangleSite", { useUnifiedTopology: true, useNewUrlParser: true })
 
+
+// creating a Schema
+const buildsSchema = {
+  title: String,
+  imageURL: String,
+  description: String
+
+};
+
+const Build = mongoose.model("Build", buildsSchema);
+
+
+const design1 = new Build ({
+  title: "SliceLine",
+  imageURL: "https://www.webdesignerdepot.com/cdn-origin/uploads/2020/01/004-1.jpg",
+  description: " This is an interesting mother fucker eh"
+});
+
+design1.save()
+
 app.get("/", function(req,res){
   res.render('home');
 });
@@ -25,13 +45,36 @@ app.get("/console", function(req,res){
 
 app.post("/console", function(req,res){
 
-})
+  const buildPost = new Build ({
+    title: req.body.title,
+    imageURL: req.body.image,
+    description: req.body.description
+  });
+
+  buildPost.save();
+  res.redirect("/showcase");
+
+});
 
 
 
 // rendering showcase view
 app.get("/showcase", function(req,res) {
-  res.render('showcase');
+
+  Build.find({}, function(err, buildsFound){
+    if(buildsFound === 0){
+      Build.insertMany(buildsFound, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully submitted blog to db");
+        }
+      });
+      res.redirect("/showcase")
+    }else{
+      res.render('showcase', {builds:buildsFound});
+    }
+  });
 });
 
 app.listen(3002, function(){
